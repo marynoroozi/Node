0e99438ffdc2818d27a8d0f4ff99735d9c7daf74
mary <noroozi.m90@gmail.com>
2025-07-22 16:02:33 -0400

    Update server to use donenv, mongoose, and flash message

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const path = require("path");
+const methodOverride = require("method-override");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const flash = require("connect-flash");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+//اگر آدرس دیگه ای میخاستم روت دابرکتوری من باشه فقط
+// require("app-module-path").addPath(__dirname + /yourDesiredFolder);
+require("app-module-path").addPath(__dirname);
+
+mongoose.connect("mongodb://127.0.0.1:27017/nodeStart");
 
 global.config = require("./config");
 
 const app = express();
-const methodOverride = require("method-override");
 
 // app.use(express.static(__dirname + "/public"));
 app.use(express.static(path.join(__dirname, "public")));
@@ -12,6 +23,16 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("method"));
+// هر رشته ای خواستیم پاس میدیم بهش به عنوان secret
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(flash());
 
 // set view
 app.set("view engine", "ejs");
