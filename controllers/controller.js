c693d09c5ea3e992e9d57787f2579cf2b62464cf
mary <noroozi.m90@gmail.com>
2025-07-24 18:51:45 -0400

    create error method in contoller

--- controllers/controller.js
+++ controllers/controller.js
@@ -6,5 +6,11 @@ class Controller {
   constructor() {
     autoBind(this);
   }
+
+  error(message, status) {
+    let err = new Error(message);
+    err.status = status;
+    throw err;
+  }
 }
 module.exports = Controller;
