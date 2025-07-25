58edec9afefb50a8d287b154364f97d3b331ccc3
mary <noroozi.m90@gmail.com>
2025-07-25 11:11:43 -0400

    Create editUser route and controller

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,10 +1,37 @@
 const contoller = require("./controller");
+const User = require("models/user");
+
 const { validationResult } = require("express-validator");
 
 class dashboardController extends contoller {
   async index(req, res, next) {
     try {
       res.render("dashboard/index");
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async editUser(req, res, next) {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        let myErrors = errors.array().map((err) => err.msg);
+        req.flash("errors", myErrors);
+        return res.redirect("/dashboard");
+      }
+      let data = {
+        name: req.body.name,
+      };
+      if (!req.file) {
+        res.redirect("/dashboard");
+      }
+      let imgPath = req.file.path.replace(/\\/g, "/").split("/public")[1];
+      data.img = imgPath;
+      await User.findByIdAndUpdate(req.user._id, {
+        $set: data,
+      });
+      res.render("dashboard/editUser");
     } catch (error) {
       next(error);
     }
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -7,14 +7,31 @@ const dashboardController = require("controllers/dashboardController");
 
 //validators
 const dashboardValidator = require("validators/dashboardValidator");
+const editUserValidator = require("validators/editUserValidator");
+
+const uploadUserProfile = require("upload/uploadUserProfile");
 
 router.use((req, res, next) => {
-  if (req.isAuthenticated()) {
-    next();
+  if (!req.isAuthenticated()) {
+    return res.redirect("/auth/login");
   }
-  return res.redirect("/");
+  next();
 });
 
 router.get("/", dashboardController.index);
+router.post(
+  "/editUser",
+  uploadUserProfile.single("img"),
+  (req, res, next) => {
+    if (!req.file) {
+      req.body.img = null;
+    } else {
+      req.body.img = req.file.filename;
+    }
+    next();
+  },
+  editUserValidator.handle(),
+  dashboardController.editUser
+);
 
 module.exports = router;
